70c7e9eca33d6a1ebe006eb89281e52c2620bce2
TheXmanz <52388234+BangDanz@users.noreply.github.com>
2020-04-28 07:02:06 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+///Coded:ApmzChannel
+////silahkan di sesuaikan sesuai kebutuhan
+$country = "ID"; //// masukan kode negara
+$type = "http"; //http atau https
+$anonymity = "transparent"; /// elite atau transparent
+?>
